f96c918da94088f1babd0563eeddedd0bd91f604
clarkngo <clarkngo@gmail.com>
2018-10-31 08:37:37 +0000

    latest

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,19 @@
 require 'homework05/version'
 
 class Person
-  # add your implementation here
+attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def to_s
+    String(@name)
+    # or just return name ?
+    # name
+  end
+
+  def titlelize
+    to_s.gsub(/\b\w/, &:capitalize)
+  end
 end
