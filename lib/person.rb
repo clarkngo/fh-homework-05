dd29b3aafa272fa3c038da81606c08a545595a47
clarkngo <clarkngo@gmail.com>
2018-11-11 23:26:06 +0000

    working

--- lib/person.rb
+++ lib/person.rb
@@ -19,31 +19,37 @@ class Person
   def titlelize
     @history << "#{@history.last}".gsub(/\b\w/, &:capitalize)
     @history.last
+    self
   end
 
   def upcase
     @history << "#{@history.last}".upcase
     @history.last
+    self
   end
 
   def downcase
     @history << "#{@history.last}".downcase
     @history.last
+    self
   end
 
   def reverse
     @history << "#{@history.last}".reverse
     @history.last
+    self
   end
 
   def hyphenize
     @history << "#{@history.last}".split("").join("-") 
     @history.last
+    self
   end  
 
   def undo
     @history.pop
     @history.last
+    self
   end  
 
 end
